cf20b3fe0d35c7010405b10ff4695315436addf3
Andrei Floricel <andrei.floricel@gmail.com>
2023-12-08 16:31:01 +0200

    Migrate to Vite (from CRA)

---
+++ src/AdaptableAgGrid/AdaptableAgGrid.tsx
@@ -0,0 +1,108 @@
+import * as React from 'react';
+import { createRoot, Root } from 'react-dom/client';
+import { useMemo } from 'react';
+import { GridOptions } from '@ag-grid-community/core';
+import { AgGridReact } from '@ag-grid-community/react';
+import { LicenseManager } from '@ag-grid-enterprise/core';
+import AdaptableReact, {
+  AdaptableApi,
+  AdaptableOptions,
+} from '@adaptabletools/adaptable-react-aggrid';
+import { columnDefs, defaultColDef } from './columnDefs';
+import { WebFramework, rowData } from './rowData';
+import { agGridModules } from './agGridModules';
+
+LicenseManager.setLicenseKey(import.meta.env.VITE_AG_GRID_LICENSE_KEY);
+
+const CONFIG_REVISION = 1;
+
+export const AdaptableAgGrid = () => {
+  const gridOptions = useMemo<GridOptions<WebFramework>>(
+    () => ({
+      defaultColDef,
+      columnDefs,
+      rowData,
+      sideBar: true,
+      statusBar: {
+        statusPanels: [
+          { statusPanel: 'agTotalRowCountComponent', align: 'left' },
+          { statusPanel: 'agFilteredRowCountComponent' },
+          {
+            key: 'Center Panel',
+            statusPanel: 'AdaptableStatusPanel',
+            align: 'center',
+          },
+        ],
+      },
+
+      suppressMenuHide: true,
+      enableRangeSelection: true,
+      enableCharts: true,
+    }),
+    []
+  );
+  const adaptableOptions = useMemo<AdaptableOptions<WebFramework>>(
+    () => ({
+      licenseKey: import.meta.env.VITE_ADAPTABLE_LICENSE_KEY,
+      primaryKey: 'id',
+      userName: 'Test User',
+      adaptableId: 'Adaptable React Support Template',
+      adaptableStateKey: 'adaptable_react_support_template',
+      predefinedConfig: {
+        Dashboard: {
+          Revision: CONFIG_REVISION,
+          Tabs: [
+            {
+              Name: 'Welcome',
+              Toolbars: ['Layout'],
+            },
+          ],
+        },
+        StatusBar: {
+          Revision: CONFIG_REVISION,
+          StatusBars: [
+            {
+              Key: 'Center Panel',
+              StatusBarPanels: ['Theme'],
+            },
+          ],
+        },
+      },
+    }),
+    []
+  );
+
+  const adaptableApiRef = React.useRef<AdaptableApi>();
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+      <AdaptableReact
+        gridOptions={gridOptions}
+        adaptableOptions={adaptableOptions}
+        renderReactRoot={renderReactRoot}
+        onAdaptableReady={({ adaptableApi }) => {
+          // save a reference to adaptable api
+          adaptableApiRef.current = adaptableApi;
+        }}
+      />
+      <div style={{ flex: 1 }} className="ag-theme-alpine">
+        <AgGridReact gridOptions={gridOptions} modules={agGridModules} />
+      </div>
+    </div>
+  );
+};
+
+/**
+ * This is required because AdapTable supports all React v16,v17 and v18 versions
+ */
+const renderWeakMap: WeakMap<HTMLElement, Root> = new WeakMap();
+function renderReactRoot(elem: React.JSX.Element, container: HTMLElement) {
+  let root = renderWeakMap.get(container);
+  if (!root) {
+    renderWeakMap.set(container, (root = createRoot(container)));
+  }
+  root.render(elem);
+  return () => {
+    root?.unmount();
+  };
+}
