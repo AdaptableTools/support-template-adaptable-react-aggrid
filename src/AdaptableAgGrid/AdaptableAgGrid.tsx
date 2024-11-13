9d2a617da9c841b06d5993c301d60079595d53ce
JonnyAdaptableTools <jonny.wolfson@adaptabletools.com>
2024-11-13 11:46:40 +0200

    Adding layout to config

--- src/AdaptableAgGrid/AdaptableAgGrid.tsx
+++ src/AdaptableAgGrid/AdaptableAgGrid.tsx
@@ -88,6 +88,32 @@ export const AdaptableAgGrid = () => {
             },
           ],
         },
+        Layout: {
+          Revision: CONFIG_REVISION,
+          CurrentLayout: 'Basic',
+          Layouts: [
+            {
+              Name: 'Basic',
+              Columns: [
+                'name',
+                'language',
+                'github_stars',
+                'license',
+                'week_issue_change',
+                'created_at',
+                'has_wiki',
+                'updated_at',
+                'pushed_at',
+                'github_watchers',
+                'description',
+                'open_issues_count',
+                'closed_issues_count',
+                'open_pr_count',
+                'closed_pr_count',
+              ],
+            },
+          ],
+        },
       },
     }),
     []
